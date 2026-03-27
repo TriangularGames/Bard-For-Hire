9a99e2cf5993d16f71a47ca153c9271415c79470
Nat <127811890+TriangularGames@users.noreply.github.com>
2026-03-26 20:05:28 -0400

    Added Descriptions and Score Editor function

--- Assets/Game/Scripts/Managers/AudienceMemberManager.cs
+++ Assets/Game/Scripts/Managers/AudienceMemberManager.cs
@@ -1,16 +1,34 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class AudienceMemberManager : MonoBehaviour
 {
     [Header("Audience Data")]
+    /// <summary>
+    /// Audience Member types available for this Performance
+    /// </summary>
     [SerializeField] List<AudienceMemberData> memberTypes;
+
+    /// <summary>
+    /// Number of Members in the Audience
+    /// </summary>
     [SerializeField] int numberOfAudienceMembers;
 
     [Header("Audience")]
+    /// <summary>
+    /// Members in the current active Audience
+    /// </summary>
     [SerializeField] List<AudienceMemberData> audienceMembers;
+
+    /// <summary>
+    /// Total Score value for this Audience
+    /// </summary>
     [SerializeField] int totalScore;
+
+    [Header("UI")]
+    [SerializeField] TMP_Text scoreToBeat;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -28,11 +46,15 @@ public class AudienceMemberManager : MonoBehaviour
         }
 
         Debug.Log("Total Score: " + totalScore);
+        scoreToBeat.text = totalScore.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Takes a Value to either add or remove from Total Score
+    /// </summary>
+    /// <param name="val">Value to add or remove</param>
+    public void EditScore(int val)
     {
-        
+        totalScore += val;
     }
 }
