f3511915eb1b625e56543caeac8ce0d9b401221a
Nat <127811890+TriangularGames@users.noreply.github.com>
2026-03-28 16:31:52 -0400

    Readjusted Note scripts and made Prefab

--- Assets/Game/Scripts/Game Objects/Notes/BaseNote.cs
+++ Assets/Game/Scripts/Game Objects/Notes/BaseNote.cs
@@ -3,14 +3,25 @@ using UnityEngine;
 /// <summary>
 /// Base note class for all notes
 /// </summary>
-public class BaseNote : INote
+public class BaseNote : MonoBehaviour, INote
 {
-    protected NoteType noteType;
+    [SerializeField] protected NoteType noteType;
     public NoteType NoteType => noteType;
+
+    [SerializeField] protected int score;
+    public int Score { get => score; set => SetScore(value); }
+
+    [SerializeField] protected bool mult;
+    public bool Mult => mult;
 
     protected virtual void Start()
     {
         // Example of how to use the NoteType property
         Debug.Log($"This note is of type: {NoteType}");
+    }
+
+    public void SetScore(int val)
+    {
+        score = val;
     }
 }
--- Assets/Game/Scripts/Interfaces/INote.cs
+++ Assets/Game/Scripts/Interfaces/INote.cs
@@ -1,10 +1,11 @@
-using UnityEngine;
-
 /// <summary>
-/// Interface for all notes in the game, defining common properties and behaviors for different note types.
+/// Interface Notes, defining common properties
 /// </summary>
 public interface INote
 {
     NoteType NoteType { get; }
 
+    int Score { get; set; }
+
+    public bool Mult { get; }
 }
