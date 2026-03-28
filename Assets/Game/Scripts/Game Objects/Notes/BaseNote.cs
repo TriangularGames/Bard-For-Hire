5a55e8ca99637e02d8cc4d8369ef7be5d30ddd49
Nat <127811890+TriangularGames@users.noreply.github.com>
2026-03-28 17:53:39 -0400

    Added summaries for vars and funcs

--- Assets/Game/Scripts/Game Objects/Notes/BaseNote.cs
+++ Assets/Game/Scripts/Game Objects/Notes/BaseNote.cs
@@ -5,22 +5,44 @@ using UnityEngine;
 /// </summary>
 public class BaseNote : MonoBehaviour, INote
 {
+    /// <summary>
+    /// This Note's NoteType
+    /// </summary>
     [SerializeField] protected NoteType noteType;
     public NoteType NoteType => noteType;
 
+    /// <summary>
+    /// This Note's Score
+    /// </summary>
     [SerializeField] protected int score;
     public int Score { get => score; set => SetScore(value); }
 
+    /// <summary>
+    /// If this Note's Score is a Mulitiplier
+    /// </summary>
     [SerializeField] protected bool mult;
     public bool Mult => mult;
 
-    protected virtual void Start()
+    /// <summary>
+    /// Die roll minmum for Note to be scored
+    /// </summary>
+    [SerializeField] protected int playable;
+    public int Playable { get => playable; set => SetPlayable(value); }
+
+/// <summary>
+/// Set the Score Value
+/// </summary>
+/// <param name="val">Value to Change to</param>
+public void SetScore(int val)
     {
-        // Example of how to use the NoteType property
-        Debug.Log($"This note is of type: {NoteType}");
+        score = val;
     }
 
-    public void SetScore(int val)
+    /// <summary>
+    /// Set the Playable Value
+    /// </summary>
+    /// <param name="val">Value to Change to</param>
+    public void SetPlayable(int val)
     {
         score = val;
     }
