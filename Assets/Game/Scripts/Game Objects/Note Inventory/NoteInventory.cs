890001c6c7cb82dade3b99ec4888f11954cec2cd
Alexander Philippopoulos <161390808+aphilippopoulos248@users.noreply.github.com>
2026-03-29 16:34:41 -0400

    Added note inventory UI

--- Assets/Game/Scripts/Game Objects/Note Inventory/NoteInventory.cs
+++ Assets/Game/Scripts/Game Objects/Note Inventory/NoteInventory.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 public class NoteInventory : MonoBehaviour
 {
     [SerializeField] private int maxSlots = 10;
+    [SerializeField] private GameObject slotPrefab;
+    [SerializeField] private Transform slotParent;
+
     private List<INote> notes;
 
     private void Awake()
     {
         notes = new List<INote>();   
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < maxSlots; i++)
+        {
+            Instantiate(slotPrefab, slotParent);
+        }
     }
 
     public void AddNote(INote note)
