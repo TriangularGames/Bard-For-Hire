91c075f54a991fab9c6d860f35d8a3c6f2835b3a
Alexander Philippopoulos <161390808+aphilippopoulos248@users.noreply.github.com>
2026-03-29 20:24:01 -0400

    Updated note pool

--- Assets/Game/Scripts/Game Objects/Note Inventory/NoteInventory.cs
+++ Assets/Game/Scripts/Game Objects/Note Inventory/NoteInventory.cs
@@ -7,54 +7,62 @@ public class NoteInventory : MonoBehaviour
     [SerializeField] private GameObject noteUIPrefab;
     [SerializeField] private Transform inventoryPanel;
 
-    private List<BaseNote> notes;
+    private List<BaseNote> Notes;
 
     private void Awake()
     {
-        notes = new List<BaseNote>();
+        Notes = new List<BaseNote>();
     }
 
+    /// <summary>
+    /// Initializing note pool
+    /// </summary>
     private void Start()
     {
         for (int i = 0; i < maxSlots; i++)
         {
             GameObject obj = Instantiate(noteUIPrefab, inventoryPanel);
-            Debug.Log("Instantiated: " + obj.name);
+            
+            BaseNote note = obj.GetComponent<BaseNote>();
+            Notes.Add(note);
         }
     }
 
-    // Adding notes to the note pool
+    /// <summary>
+    /// Adding new notes to note pool
+    /// </summary>
+    /// <param name="note"></param>
     public void AddNote(BaseNote note)
     {
+        GameObject obj = Instantiate(noteUIPrefab, inventoryPanel);
+
         if (note == null)
         {
             Debug.LogWarning("Tried to add a null note.");
             return;
         }
-
-        if (notes.Count >= maxSlots)
+        if (Notes.Count >= maxSlots)
         {
             Debug.Log("Inventory is full.");
             return;
         }
-
-        if (notes.Contains(note))
+        if (Notes.Contains(note))
         {
             Debug.Log("This note is already in the inventory.");
             return;
         }
-
-        notes.Add(note);
-
-        GameObject newUI = Instantiate(noteUIPrefab, inventoryPanel);
+        Notes.Add(note);
     }
 
-    // Used when discarding notes in the note pool
+   /// <summary>
+   /// Removing notes from note pool when player discards
+   /// </summary>
+   /// <param name="note"></param>
     public void RemoveNote(BaseNote note)
     {
         if (note == null) return;
 
-        if (notes.Remove(note))
+        if (Notes.Remove(note))
         {
             Debug.Log($"Removed {note.name} from inventory.");
         }
