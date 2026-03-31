eac7ff50a6a2b01499ee09e344750ae92d840c98
Alexander Philippopoulos <161390808+aphilippopoulos248@users.noreply.github.com>
2026-03-30 22:44:22 -0400

    Updated inventory scene

---
+++ Assets/Game/Scripts/Game Objects/Inventory/NoteInventory.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteInventory : MonoBehaviour
+{
+    [SerializeField] private int inventorySpace = 10;
+    [SerializeField] public Transform inventoryPanel;
+
+    // This is just for testing
+    [SerializeField] public GameObject notePrefab;
+
+    private List<GameObject> noteInventory;
+
+    public List<GameObject> GetNoteInventory()
+    {
+        return noteInventory;
+    }
+
+    private void Awake()
+    {
+        noteInventory = new List<GameObject>();
+    }
+
+    /// <summary>
+    /// Initializing note inventory
+    /// </summary>
+    private void Start()
+    {
+        if (inventoryPanel == null) return;
+
+        for (int i = 0; i < inventorySpace; i++)
+        {
+            GameObject note = Instantiate(notePrefab, inventoryPanel);
+            note.name = note.name + i.ToString();
+
+            int noteToCreate = Random.Range(0, ResourceManager.Instance.NoteData.Length);
+            note.GetComponent<NoteController>().noteData = ResourceManager.Instance.NoteData[noteToCreate];
+
+            note.GetComponent<NoteController>().SetSprite();
+
+            AddNote(note);
+        }
+    }
+
+    /// <summary>
+    /// Adding new notes to note inventory
+    /// </summary>
+    /// <param name="note">Note data to add</param>
+    public void AddNote(GameObject note)
+    {
+        if (inventoryPanel == null) return;
+
+        if (note == null)
+        {
+            Debug.LogWarning("Tried to add a null note.");
+            return;
+        }
+        if (noteInventory.Count >= inventorySpace)
+        {
+            Debug.Log("Inventory is full.");
+            return;
+        }
+        if (noteInventory.Contains(note))
+        {
+            Debug.Log("This note is already in the inventory.");
+            return;
+        }
+        noteInventory.Add(note);
+    }
+}
