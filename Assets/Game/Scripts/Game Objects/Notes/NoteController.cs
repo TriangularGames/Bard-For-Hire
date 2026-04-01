1fd43db6a3b12f46a470be23842484209d6363cd
Nat <ncordeiro662@gmail.com>
2026-04-01 08:02:04 -0400

    Added new Setup funcs
    Created a public Setup func to do the display for the Note sprite, score, and playable values

--- Assets/Game/Scripts/Game Objects/Notes/NoteController.cs
+++ Assets/Game/Scripts/Game Objects/Notes/NoteController.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,8 +6,36 @@ public class NoteController : MonoBehaviour
 {
     [SerializeField] public NoteData noteData;
 
-    public void SetSprite()
+    [SerializeField] private TMP_Text scoreTxt;
+    [SerializeField] private TMP_Text playableText;
+
+    public void Setup()
+    {
+        SetSprite();
+        SetScoreTxt();
+        SetPlayableTxt();
+    }
+
+    private void SetSprite()
     {
         GetComponent<Image>().sprite = noteData.icon;
+    }
+
+    private void SetScoreTxt()
+    {
+        if (noteData.Mult)
+        {
+            scoreTxt.text = "x" + noteData.Score.ToString();
+        }
+        else
+        {
+            scoreTxt.text = "+" + noteData.Score.ToString();
+        }
+        
+    }
+
+    private void SetPlayableTxt()
+    {
+        playableText.text = "(" + noteData.Playable.ToString() + ")";
     }
 }
