9c25a2ae0197a9d1ebab34f1d826433512b98c01
Nat <127811890+TriangularGames@users.noreply.github.com>
2026-04-28 12:30:03 -0400

    Added MainMenuUI script

---
+++ Assets/Game/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void Play()
+    {
+        // TODO: change this to prompt player for the tutorial, or just go to the Starter Shop screen
+        SceneLoader.Instance.LoadScene("Inventory", LoadSceneMode.Single);
+        //GameManager.Instance.SwitchState(new NewGameState());
+    }
+
+    public void Options()
+    {
+        MenuManager.Instance.SwitchState(new OptionsMenuState());
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
