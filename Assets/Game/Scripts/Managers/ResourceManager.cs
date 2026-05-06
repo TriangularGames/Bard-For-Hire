97933e0b3b8ac7a150e0d1a0fb9002c29d386e8f
Nat <127811890+TriangularGames@users.noreply.github.com>
2026-05-06 13:52:03 -0400

    Added UpgradeData

--- Assets/Game/Scripts/Managers/ResourceManager.cs
+++ Assets/Game/Scripts/Managers/ResourceManager.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class ResourceManager : Singleton<ResourceManager>
 {
     public NoteData[] NoteData;
+    public UpgradeData[] UpgradeData;
     public AudienceMemberData[] AudienceMemberData;
+
     private void Start()
     {
         NoteData = Resources.LoadAll<NoteData>("ScriptableObjects/Notes");
+        UpgradeData = Resources.LoadAll<UpgradeData>("ScriptableObjects/Upgrades");
         AudienceMemberData = Resources.LoadAll<AudienceMemberData>("ScriptableObjects/Audience");
     }
 }
