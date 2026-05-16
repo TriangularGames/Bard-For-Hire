0f313b0424496602825dda343c07af01bc52f1e4
Alexander Philippopoulos <161390808+aphilippopoulos248@users.noreply.github.com>
2026-05-15 20:39:13 -0400

    Updated pause menu

--- Assets/Game/Scripts/Interfaces/GameStates/CombatState.cs
+++ Assets/Game/Scripts/Interfaces/GameStates/CombatState.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class CombatState : IGameState
 {
+    
     public void EnterState(GameManager gm)
     {
         SceneLoader.Instance.LoadScene("Combat", UnityEngine.SceneManagement.LoadSceneMode.Single);
