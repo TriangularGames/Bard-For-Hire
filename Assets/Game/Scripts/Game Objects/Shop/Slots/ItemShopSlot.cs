1384a93d892d31a8c5d091ef367c772c657f5dcb
Nat <127811890+TriangularGames@users.noreply.github.com>
2026-05-28 20:40:28 -0400

    Fixed Buy Button Interactability

--- Assets/Game/Scripts/Game Objects/Shop/Slots/ItemShopSlot.cs
+++ Assets/Game/Scripts/Game Objects/Shop/Slots/ItemShopSlot.cs
@@ -26,6 +26,14 @@ public class ItemShopSlot : ShopSlot
         _data = null;
     }
 
+    private void Update()
+    {
+        if (_data != null && PlayerManager.Instance.GetCoinAmount() < _data.cost)
+        {
+            buy.interactable = false;
+        }
+    }
+
     public override void SelectSlot(bool select)
     {
         if (_data != null)
