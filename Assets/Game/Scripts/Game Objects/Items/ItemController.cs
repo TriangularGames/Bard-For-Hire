08202f8e86b744f3a5ea11666060dd9b8b300e3a
Alexander Philippopoulos <161390808+aphilippopoulos248@users.noreply.github.com>
2026-06-05 14:03:30 -0400

    Fixed upgrade display issue

--- Assets/Game/Scripts/Game Objects/Items/ItemController.cs
+++ Assets/Game/Scripts/Game Objects/Items/ItemController.cs
@@ -72,6 +72,18 @@ public class ItemController : MonoBehaviour
         
     }
 
+    public void SetDamageDisplay(int value)
+    {
+        damageTxt.text = "Dmg " + value;
+    }
+    public void SetDamageWithBonus(int baseD, UpgradeFightingManager.DamageBonus bonus)
+    {
+        if (bonus.amount > 0)
+            damageTxt.text = $"Dmg {baseD}  <color=yellow>+ {bonus.amount} {bonus.source}</color>";
+        else if (bonus.amount < 0)
+            damageTxt.text = $"Dmg {baseD}  <color=red>{bonus.amount} {bonus.source}</color>";
+    }
+
     private void SetPlayableTxt()
     {
         playableText.text = "Roll (" + itemData.Playable.ToString() + ")";
